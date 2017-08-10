5f95f23806864b8abbed494522381bf460b01a77
Thai Bui <blquythai@hotmail.com>
2017-08-10 15:58:05 -0500

    Add configure supervisord cmd

--- tools/ops/install_centos-rhel_prerequisites.sh
+++ tools/ops/install_centos-rhel_prerequisites.sh
@@ -10,9 +10,11 @@ tar -xzvf /tmp/apache-maven-$MAVEN_VERSION-bin.tar.gz -C /opt
 export PATH=/opt/apache-maven-$MAVEN_VERSION/bin:$PATH
 
 # Install all required external packages including supervisor
-sudo yum -y install ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain gcc gcc-c++ \
+yum -y install ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain gcc gcc-c++ \
     krb5-devel libffi-devel libxml2-devel libxslt-devel make mysql mysql-devel openldap-devel \
     python-devel sqlite-devel gmp-devel openssl-devel supervisor git
 
 # Install supervisord for easy hue process management
 easy_install supervisor
+# Configure, require root access
+echo_supervisord_conf > /etc/supervisord.conf
