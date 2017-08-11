c308b76606737b390453d775e23d04466a6ee14e
Thai Bui <blquythai@hotmail.com>
2017-08-11 17:57:46 -0500

    Force randomization of param in source install.sh

--- tools/ops/install.sh
+++ tools/ops/install.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Install Centos 6 prerequisites
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/install_centos-rhel_prerequisites.sh)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/install_centos-rhel_prerequisites.sh?$RANDOM)
 
 # Pre-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/pre-install.sh)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/pre-install.sh?$RANDOM)
 
 # Install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/install_hue_centos-rhel.sh)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/install_hue_centos-rhel.sh?$RANDOM)
 
 # Post-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/post-install.sh)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/master/tools/ops/post-install.sh?$RANDOM)
