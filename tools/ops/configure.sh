4c64999436949e8a1f20a2521497c890cc39b8bc
Thai Bui <blquythai@hotmail.com>
2017-08-11 17:45:50 -0500

    Add configure script

---
+++ tools/ops/configure.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -e
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    --hue-dir)
+    HUE_DIR="$2"
+    shift # past argument
+    ;;
+    --master-hostname)
+    MASTER="$2"
+    shift # past argument
+    ;;
+    --ldap-server)
+    LDAP_SERVER="$2"
+    shift # past argument
+    ;;
+    --ldap-base-dn)
+    LDAP_BASE_DN="$2"
+    shift # past argument
+    ;;
+    *)
+    echo "Unknown option $key"
+    exit -1
+    ;;
+esac
+shift
+done
+
+# Get hue install dir
+if [ -z "$HUE_DIR" ]; then
+    echo "Hue install dir not supplied. Default to /opt/hue"
+    HUE_DIR=/opt/hue
+fi
+
+if [ -z "$MASTER" ]; then
+    echo "Master hostname not supplied. Default to `hostname`"
+    MASTER=`hostname`
+fi
+
+if [ -z "$LDAP_SERVER" ]; then
+    echo "ldap server is required"
+    exit -1
+fi
+
+if [ -z "$LDAP_BASE_DN" ]; then
+    echo "ldap base dn is required"
+    exit -1
+fi
+
+echo "Hue installed dir: $HUE_DIR"
+echo "Master hostname: $MASTER"
+echo "LDAP server: $LDAP_SERVER"
+echo "LDAP base dn: $LDAP_BASE_DN"
+
+echo "Configuring .. $HUE_DIR/desktop/conf/pseudo-distributed.ini"
+cat $HUE_DIR/desktop/conf/pseudo-distributed.ini.ambari.tmpl | \
+    sed "s/{{master}}/$MASTER/g" | \
+    sed "s/{{ldap-server}}/$LDAP_SERVER/g" | \
+    sed "s/{{ldap-base-dn}}/$LDAP_BASE_DN/g" \
+    > $HUE_DIR/desktop/conf/pseudo-distributed.ini
