613b6aab4bce98a7c7632ddc2a20293392f71ca8
Thai Bui <blquythai@hotmail.com>
2017-10-05 00:31:19 -0500

    Fix problem with installing Hue the latest image

--- tools/ops/install_centos-rhel_prerequisites.sh
+++ tools/ops/install_centos-rhel_prerequisites.sh
@@ -9,11 +9,16 @@ wget http://apache.mirrors.ionfish.org/maven/maven-3/$MAVEN_VERSION/binaries/apa
 tar -xzvf /tmp/apache-maven-$MAVEN_VERSION-bin.tar.gz -C /opt
 export PATH=/opt/apache-maven-$MAVEN_VERSION/bin:$PATH
 
+# Ambari metrics uses gcc64 which messes up Hue using gcc48, so we uninstall gcc64 (which include ambari metrics monitor)
+# then install it back at the end.
+# TODO just make an RPM for Hue already!
+yum -y remove gcc64
+
 # Install all required external packages including supervisor
-# gcc gcc-c++ \ gcc and gcc-++ is no longer needed in the latest image installing it messes up things
-yum -y install ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain \
+yum -y install ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain gcc gcc-c++ \
     krb5-devel libffi-devel libxml2-devel libxslt-devel make mysql mysql-devel openldap-devel \
-    python-devel sqlite-devel gmp-devel openssl-devel postgresql-devel supervisor git jq
+    python-devel sqlite-devel gmp-devel openssl-devel postgresql-devel supervisor git jq \
+    ambari-metrics-monitor
 
 # Install supervisord for easy hue process management
 easy_install supervisor
