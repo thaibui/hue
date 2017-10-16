34d98bc7702901ce7c6f3b574aacccabab79bfbe
Sudhakar Vasiraju <Sudhakar.Vasiraju@bazaarvoice.com>
2017-10-16 16:19:10 -0500

    modification - 1

---
+++ deploy/rpm/post-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+cp /opt/hue/tools/ops/hued /etc/init.d/
+
+chkconfig --add hued
+
+# Add log dir
+mkdir -p /var/log/hue
+chown -R hue:hue /var/log/hue
+
+# Add directory for pidfile
+mkdir -p /var/run/hue
+chown -R hue:hue /var/run/hue
+
+# Add hue permissions to hue working directory
+chown -R hue:hue /opt/hue
