3ea4e078d09a572faf138a8d5a0a86ab2594f4c8
Thai Bui <blquythai@hotmail.com>
2017-12-20 18:09:51 -0600

    add hue portal path to install and post install script

--- tools/ops/install.sh
+++ tools/ops/install.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Install Centos 6 prerequisites
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal/tools/ops/install_centos-rhel_prerequisites.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal-old-vpc/tools/ops/install_centos-rhel_prerequisites.sh?$RANDOM)
 
 # Pre-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal/tools/ops/pre-install.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal-old-vpc/tools/ops/pre-install.sh?$RANDOM)
 
 # Install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal/tools/ops/install_hue_centos-rhel.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal-old-vpc/tools/ops/install_hue_centos-rhel.sh?$RANDOM)
 
 # Post-install
-source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal/tools/ops/post-install.sh?$RANDOM)
+source <(curl -s https://raw.githubusercontent.com/thaibui/hue/hue-portal-old-vpc/tools/ops/post-install.sh?$RANDOM)
